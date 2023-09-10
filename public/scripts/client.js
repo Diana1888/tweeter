4611bbc3a286a6e887640754106f71116f0c3887
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-09-10 00:39:31 -0400

    Add toggle btn sliding up and down tweet form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
-
 $(document).ready(function() {
   //Preventing XSS with Escaping
   const escape = function (str) {
@@ -39,7 +11,6 @@ $(document).ready(function() {
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
-
 
 
   //Create the tweet element
@@ -69,6 +40,13 @@ $(document).ready(function() {
 
     return $tweet;
   };
+
+  $('#toggle-btn').on('click', (event) => {
+    event.preventDefault();
+    $('.new-tweet').toggle(function() {
+      $('#tweet-text').trigger('focus');
+    });
+  })
 
 
   $('.tweet-form').on('submit', (event) => {
