5e07b8ea4100aacff6a36b1979fea844fead1c63
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-09-14 22:39:58 -0400

    Add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
   const $tweetText = $('#tweet-text');
+  //count characters dynamically as the user types
   $tweetText.on('input', function() {
     const $textValue = $tweetText.val();
     let $startCounter = 140;
@@ -9,6 +10,7 @@ $(document).ready(function() {
     const $charCount = `${$startCounter - $textValue.length}`;
     $counter.text(`${$charCount}`);
 
+    //counter turns red if exceed allowed characters
     if ($charCount < 0) {
       $counter.addClass('negative')
     } else {
